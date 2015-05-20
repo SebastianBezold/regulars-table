161ebbe253273cf4d4554039227bf621eccb50b5
Sebastian Bezold <SebastianBezold@googlemail.com>
2015-05-20 21:13:28 +0200

    Small formatting

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,5 +3,6 @@ class Employee < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   has_and_belongs_to_many :teams
 end
