84ceeaeea83bbc9c1ac5a22f6c25787c83ebff67
Sebastian Bezold <SebastianBezold@googlemail.com>
2015-05-20 20:27:26 +0200

    Add devise functionality to Employee

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150520181001) do
+ActiveRecord::Schema.define(version: 20150520182348) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "text"
@@ -27,9 +27,22 @@ ActiveRecord::Schema.define(version: 20150520181001) do
   create_table "employees", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
+
+  add_index "employees", ["email"], name: "index_employees_on_email", unique: true
+  add_index "employees", ["reset_password_token"], name: "index_employees_on_reset_password_token", unique: true
 
   create_table "employees_teams", id: false, force: :cascade do |t|
     t.integer "employee_id", null: false
