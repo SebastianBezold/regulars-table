ade4eac9a83381ae0a4bc2d3cbd5bb844b95d6e9
Sebastian Bezold <SebastianBezold@googlemail.com>
2015-05-24 14:02:40 +0200

    Add create method to invitation controller

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,2 +1,19 @@
 class InvitationsController < ApplicationController
+
+  def create
+    @invitation = Invitation.new
+    @invitation.status.unknown!
+
+    respond_to do |format|
+      if @invitation.save
+        format.html { redirect_to @invitation, notice: 'invitation was successfully created.' }
+        format.json { render :show, status: :created, location: @invitation }
+      else
+        format.html { render :new }
+        format.json { render json: @invitation.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
 end
