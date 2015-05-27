b2a6c2334ce594d50364e6a21d13c8be2a52e5c9
Sebastian Bezold <Sebastian.Bezold@cas.de>
2015-05-27 14:33:57 +0200

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+employees = Employee.create([
+  {
+    first_name: 'Sebastian',
+    last_name: 'Bezold',
+    email: 'SebastianBezold@googlemail.com',
+    password: 'Sebastian',
+    password_confirmation: 'Sebastian'
+  },
+  {
+    first_name: 'Michael',
+    last_name: 'Sprauer',
+    email: 'Michael@sprauer.net',
+    password: 'MichaelSp',
+    password_confirmation: 'MichaelSp'
+  }])
+
+teams = Team.create([
+  {
+    name: 'DHBW'
+  },
+  {
+    name: 'CAS'
+  }])
+
+events = Event.create([
+  {
+    name: 'Rails coding session',
+    date: DateTime.strptime("05/30/2015 17:00", "%m/%d/%Y %H:%M"),
+    location: 'DHBW meeting room',
+    creator: employees.first # creator
+  }])
+
+sebastian = employees.first
+sebastian.teams = teams
+sebastian.save
+
+michael = employees.second
+michael.team_ids = [teams.first.id]
+michael.save
