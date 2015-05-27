80ad5ba48c75224187a4ec295dd84c7e6949d38c
Sebastian Bezold <Sebastian.Bezold@cas.de>
2015-05-27 13:59:17 +0200

    Add password-field to employee _form

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -69,6 +69,6 @@ class EmployeesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def employee_params
-      params.require(:employee).permit(:first_name, :last_name, :team_ids => [])
+      params.require(:employee).permit(:first_name, :last_name, :password, :team_ids => [])
     end
 end
